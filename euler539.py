7e615ae5bd320e51f6e29262fa5c5f46cbc86d54
Natasha Mathur <natashamathur@uchicago.edu>
2018-12-05 03:31:27 -0600

    Create euler539.py

---
+++ euler539.py
@@ -0,0 +1,39 @@
+# https://projecteuler.net/problem=539
+
+n = 1000
+
+def get_p(n):
+    vals = [i for i in range(1, n+1)]
+
+    start_from_left = True
+
+    while len(vals) > 1:
+        new_list = []
+        length = int(len(vals))
+        if start_from_left == True:
+            for v in range(1, length,2):
+                num = int(vals[v])
+                new_list.append(num)
+        elif start_from_left == False:
+            for v in range(length-2, -1, -2):
+                num = vals[v]
+                new_list.append(num)
+                
+        start_from_left = not start_from_left
+        new_list.sort()
+        vals = new_list
+    
+    return vals[0]
+
+def get_s(n):
+
+    total = 0
+    for i in range(1, n + 1):
+        total += get_p(i)
+
+    return total
+
+x = 10 ** 18
+s = get_s(x)
+
+print(s % 987654321)
