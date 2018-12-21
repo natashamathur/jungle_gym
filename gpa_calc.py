0cf9677d5bf86717600411b184e245004a56db7a
Natasha Mathur <natashamathur@uchicago.edu>
2018-12-21 00:54:59 -0500

    updates

--- gpa_calc.py
+++ gpa_calc.py
@@ -1,4 +1,4 @@
-# To run using the command line enter a command in this format: python3 calc.py "['B','A-', 'B+', 'B+']"
+# To run using the command line enter a command in this format: python3 gpa_calc.py "A- A B"
 
 import argparse
 
@@ -28,23 +28,12 @@ def gpa_calc(grades=grades, gpa_scale = gpa_scale, inc = 0.3, round_gpa = 2):
     if round_gpa:
         return round((total/len(grades)), 2)
     else:
-        return (total/len(grades)
-  
-
-def get_grades_list(g):
-    # Turn argparse argument into a list
-    
-    g = g.split(",")
-    g = [x.strip("[]") for x in g]
-    g = [x.strip(" ") for x in g]
-    g = [x.strip("'") for x in g]
-    return g
+        return (total/len(grades))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get GPA')
     parser.add_argument('grades')
     args = parser.parse_args()
-    grades_list = get_grades_list(args.grades)
-    print(gpa_calc(grades_list))
+    print(gpa_calc(args.grades.split()))
     
