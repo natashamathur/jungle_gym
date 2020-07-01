d406d2e22cb9a07c88c6d90d1625cfdf18580023
Natasha <natashamathur@Natashas-MacBook-Air.local>
2020-07-01 01:44:12 -0400

    formatted code and numbers

--- common_word.py
+++ common_word.py
@@ -1,31 +1,187 @@
 import sys
 from string import punctuation
 
-def strip_punctuation(s):
-    
-    return ''.join(c for c in s if c not in punctuation)
 
-def common_words(x, n=5, more_stops = [], remove_stop_words = True):
-    '''
+def strip_punctuation(s):
+
+    return "".join(c for c in s if c not in punctuation)
+
+
+def common_words(x, n=5, more_stops=[], remove_stop_words=True):
+    """
     Takes in a .txt file or a string and returns the most commonly used word. 
-    '''
+    """
 
     if type(x) is not str:
-        x = open(x, 'r', encoding = 'latin-1')
+        x = open(x, "r", encoding="latin-1")
         x = x.read()
 
     x = strip_punctuation(x)
     lx = x.lower().split()
-    
-    STOP_WORDS = [ "a", "about", "above", "after", "again", "nan", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
 
-    
+    STOP_WORDS = [
+        "a",
+        "about",
+        "above",
+        "after",
+        "again",
+        "nan",
+        "against",
+        "all",
+        "am",
+        "an",
+        "and",
+        "any",
+        "are",
+        "as",
+        "at",
+        "be",
+        "because",
+        "been",
+        "before",
+        "being",
+        "below",
+        "between",
+        "both",
+        "but",
+        "by",
+        "could",
+        "did",
+        "do",
+        "does",
+        "doing",
+        "down",
+        "during",
+        "each",
+        "few",
+        "for",
+        "from",
+        "further",
+        "had",
+        "has",
+        "have",
+        "having",
+        "he",
+        "he'd",
+        "he'll",
+        "he's",
+        "her",
+        "here",
+        "here's",
+        "hers",
+        "herself",
+        "him",
+        "himself",
+        "his",
+        "how",
+        "how's",
+        "i",
+        "i'd",
+        "i'll",
+        "i'm",
+        "i've",
+        "if",
+        "in",
+        "into",
+        "is",
+        "it",
+        "it's",
+        "its",
+        "itself",
+        "let's",
+        "me",
+        "more",
+        "most",
+        "my",
+        "myself",
+        "nor",
+        "of",
+        "on",
+        "once",
+        "only",
+        "or",
+        "other",
+        "ought",
+        "our",
+        "ours",
+        "ourselves",
+        "out",
+        "over",
+        "own",
+        "same",
+        "she",
+        "she'd",
+        "she'll",
+        "she's",
+        "should",
+        "so",
+        "some",
+        "such",
+        "than",
+        "that",
+        "that's",
+        "the",
+        "their",
+        "theirs",
+        "them",
+        "themselves",
+        "then",
+        "there",
+        "there's",
+        "these",
+        "they",
+        "they'd",
+        "they'll",
+        "they're",
+        "they've",
+        "this",
+        "those",
+        "through",
+        "to",
+        "too",
+        "under",
+        "until",
+        "up",
+        "very",
+        "was",
+        "we",
+        "we'd",
+        "we'll",
+        "we're",
+        "we've",
+        "were",
+        "what",
+        "what's",
+        "when",
+        "when's",
+        "where",
+        "where's",
+        "which",
+        "while",
+        "who",
+        "who's",
+        "whom",
+        "why",
+        "why's",
+        "with",
+        "would",
+        "you",
+        "you'd",
+        "you'll",
+        "you're",
+        "you've",
+        "your",
+        "yours",
+        "yourself",
+        "yourselves",
+    ]
+
     if len(more_stops) > 0:
         STOP_WORDS = STOP_WORDS + more_stops
-        
+
     if remove_stop_words:
-         lx = [w for w in lx if w not in STOP_WORDS]
-    
+        lx = [w for w in lx if w not in STOP_WORDS]
+
     d = {}
     for word in lx:
         if word not in d.keys():
@@ -33,17 +189,19 @@ def common_words(x, n=5, more_stops = [], remove_stop_words = True):
         else:
             d[word] += 1
 
-    most_common = sorted(d, key = d.get, reverse=True)[:n]
-            
-    #print("The " + str(n) + " most common words are: " + ( ", ".join( e for e in most_common)) + ".")
+    most_common = sorted(d, key=d.get, reverse=True)[:n]
+
+    # print("The " + str(n) + " most common words are: " + ( ", ".join( e for e in most_common)) + ".")
     return most_common
+
 
 import json
 
+
 def str_fb_comments(jsonfile):
-    '''
+    """
     Gets string of all facebook comments from Facebook-provided data. 
-    '''
+    """
 
     f = open(jsonfile)
     data = json.load(f)
@@ -51,24 +209,25 @@ def str_fb_comments(jsonfile):
     comments = []
     for c in range(len(data)):
         try:
-            temp = data[c]['data'][0]['comment']['comment']
+            temp = data[c]["data"][0]["comment"]["comment"]
             comments.append(temp)
         except:
             # ignore comment as this is an attachment, not text
             pass
-    
-    s = ''
+
+    s = ""
     for c in comments:
         s = s + " " + c
 
     return s
 
-s = str_fb_comments('comments.json')
+
+s = str_fb_comments("comments.json")
 
 print(cw.common_words(s, n=10))
 
-'''
+"""
 Here are the results for my facebook comments!
 
 The 10 most common words are: not, im, like, just, dont, know, thats, thank, now, can.
-'''
+"""
--- tennis.py
+++ tennis.py
@@ -1,19 +1,33 @@
 import random
 
-class Player(object):
 
+class Player(object):
     def __init__(self, name):
         self.points_won = 0
         self.games_won = 0
         self.sets_won = 0
-        self.score = str(self.sets_won) + " sets " + str(self.games_won) + " games " + str(self.points_won) + " points "
+        self.score = (
+            str(self.sets_won)
+            + " sets "
+            + str(self.games_won)
+            + " games "
+            + str(self.points_won)
+            + " points "
+        )
         self.name = name
 
     def __repr__(self):
         return self.name + " " + self.score
 
     def set_score(self, score):
-        self.score = str(self.sets_won) + " sets " + str(self.games_won) + " games " + str(self.points_won) + " points "
+        self.score = (
+            str(self.sets_won)
+            + " sets "
+            + str(self.games_won)
+            + " games "
+            + str(self.points_won)
+            + " points "
+        )
 
     def add_point(self):
 
@@ -25,17 +39,15 @@ class Player(object):
             self.points_won = "Adv"
         elif self.points_won == "Adv":
             self.points_won = 0
-            self.games_won += 1                                   
+            self.games_won += 1
             if self.games_won >= 6:
                 self.sets_won += 1
                 self.games_won = 0
                 self.points_won = 0
         self.set_score(self.score)
 
-        
-            
-class Match(object):
 
+class Match(object):
     def __init__(self, player1, player2):
         self.player1 = Player(player1)
         self.player2 = Player(player2)
@@ -55,7 +67,6 @@ class Match(object):
                 break
             self.win_point()
 
-           
     def win_point(self):
         decider = random.randint(1, 2)
         if decider == 1:
@@ -63,7 +74,7 @@ class Match(object):
             return self.player1.name
         if decider == 2:
             self.player2.add_point()
-            return self.player2.name 
+            return self.player2.name
 
     def who_won(self):
         if self.player1.sets_won > self.player2.sets_won:
@@ -72,14 +83,11 @@ class Match(object):
             self.winner = self.player2
 
         return self.winner
-    
+
+
 # trial
 
 x = Match("Nadal", "Federer")
 x.simulate_match()
 x.who_won()
 print(x.who_won())
-
-
-            
-        
