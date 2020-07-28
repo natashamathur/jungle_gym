e3d87f65de6ef0a96afbf68fd736f559502a7d24
Natasha <35498283+natashamathur@users.noreply.github.com>
2020-07-28 01:07:55 -0400

    Update twilio_trial.py

--- money_on_my_mind/twilio_trial.py
+++ money_on_my_mind/twilio_trial.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
 from functions_for_budget_tracker import *
 
+# https://www.twilio.com/blog/2016/09/how-to-receive-and-respond-to-a-text-message-with-python-flask-and-twilio.html
 # first run ./ngrok http 5000 in a different tab
 # then run this
 # put ngrok address in https://www.twilio.com/console/phone-numbers
